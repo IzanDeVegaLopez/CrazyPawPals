233b7f1783316c7b1e22ce7927d05915b22fe1cb
Haoshuang <haohou@ucm.es>
2025-02-13 20:41:42 +0100

    .

--- project/src/our_scripts/Bullet.cpp
+++ project/src/our_scripts/Bullet.cpp
@@ -6,9 +6,9 @@
 Bullet::Bullet(ecs::Manager* mngr,Vector2D& pos, Vector2D& dir, float speed, float lifeTime)
 	:GameObject::GameObject(mngr,ecs::grp::BULLET)
 {
-	_tr = _mngr->getComponent<Transform>(_entity);
+	_tr = _entity->getMngr()->getComponent<Transform>(_entity);
 	_tr->init(pos, dir, 40.0f, 40.0f, 0.0f, speed);
-	_mngr->addComponent<Image>(_entity, &sdlutils().images().at("pacman"));
-	_mngr->addComponent<SimpleMove>(_entity);
-	_mngr->addComponent<LifetimeTimer>(_entity,lifeTime);
+	_entity->getMngr()->addComponent<Image>(_entity, &sdlutils().images().at("pacman"));
+	_entity->getMngr()->addComponent<SimpleMove>(_entity);
+	_entity->getMngr()->addComponent<LifetimeTimer>(_entity,lifeTime);
 }
