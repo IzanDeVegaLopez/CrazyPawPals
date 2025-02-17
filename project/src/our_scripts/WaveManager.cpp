936cf1a8aa9e5cdd43e78ef7c3a6a98a8926000d
amielrm4 <amielr01@ucm.es>
2025-02-17 16:47:07 +0100

    WAVE_MANAGER para cada enemigo
    (sin spawnear enemigos)

--- project/src/our_scripts/WaveManager.cpp
+++ project/src/our_scripts/WaveManager.cpp
@@ -1,64 +1,85 @@
-//#include <vector>
-//#include "WaveManager.h"
-//#include "EnemyPoolManager.h"
-//#include "ecs/Manager.h"
-//#include "sdlutils/SDLUtils.h"
-//
-//WaveManager::WaveManager(ecs::Manager* mngr, EnemyPoolManager* enemyPool)
-//    : _mngr(mngr), _enemyPool(enemyPool), _lastSpawnTime(0), _spawnInterval(60000), _waveActive(false), _fogActive(false) {}
-//
-//
-//void 
-//WaveManager::update() {
-//    Uint32 currentTime = SDL_GetTicks();
-//
-//    if (_waveActive) {
-//        // Verificar si ha pasado un minuto desde el último spawn
-//        if (currentTime - _lastSpawnTime >= _spawnInterval) {
-//            if (!areAllEnemiesDead()) {
-//                activateFog(); // Activar la niebla si no se han eliminado todos los enemigos
-//            }
-//            else {
-//                enterRewardsMenu(); // Ir al menú de recompensas si se eliminaron todos los enemigos
-//            }
-//            _waveActive = false; // Finalizar la oleada
-//        }
-//    }
-//    else {
-//        // Iniciar una nueva oleada
-//        spawnWave();
-//        _waveActive = true;
-//        _lastSpawnTime = currentTime;
-//    }
-//}
-//
-//// Spawnear enemigos fuera de la pantalla
-//void 
-//WaveManager::spawnWave() {
-//    for (int i = 0; i < 10; ++i) { // 10 por ahora
-//        float x = -100.0f; // Fuera de la pantalla a la izquierda
-//        float y = sdlutils().rand().nextInt(0, sdlutils().height()); // Posición Y aleatoria
-//        //_enemyPool->addEnemies(x, y);
-//    }
-//}
-//
-////Verifica si todos los enemigos estan muertos
-//bool 
-//WaveManager::areAllEnemiesDead() {
-//    auto activeEnemies = _enemyPool->getActiveObjects();
-//    return activeEnemies.empty();
-//}
-//
-////Activa la niebla
-//void 
-//WaveManager::activateFog() {
-//    _fogActive = true;
-//    std::cout << "Niebla activada!" << std::endl;
-//}
-//
-//
-//void 
-//WaveManager::enterRewardsMenu() {
-//    std::cout << "Todos los enemigos eliminados. Entrando al menú de recompensas..." << std::endl;
-//}
-//
+#include <vector>
+#include "WaveManager.h"
+#include "EnemyPoolManager.h"
+#include "ecs/Manager.h"
+#include "sdlutils/SDLUtils.h"
+
+WaveManager::WaveManager(ecs::Manager* mngr, EnemyPoolManager* enemyPool)
+    : _mngr(mngr), _enemyPool(enemyPool),
+    _totalSpawnTime(10000.f), _waveTime(60000 - _totalSpawnTime),
+    _numEnemies(10), 
+    _waveActive(false), _fogActive(false) {}
+
+void 
+WaveManager::update() {
+    Uint32 currentTime = SDL_GetTicks();
+
+    if (_waveActive) {
+        // Verificar si ha pasado un minuto desde el ï¿½ltimo spawn
+        if (currentTime > _endWaveTime) {
+            if (!areAllEnemiesDead()) {
+                activateFog(); // Activar la niebla si no se han eliminado todos los enemigos
+            }
+            else {
+                enterRewardsMenu(); // Ir al menï¿½ de recompensas si se eliminaron todos los enemigos
+            }
+            _waveActive = false; // Finalizar la oleada
+        }
+    }
+    else {
+        // Iniciar una nueva oleada
+        spawnWave();
+    }
+}
+
+// Spawnear enemigos fuera de la pantalla
+void 
+WaveManager::spawnWave() {
+    srand(time(0)); //hay que poner esto en el principio del programa una sola vez
+    
+    if (_enemiesSpawned < _numEnemies) {
+        if (currentTime > _nextSpawn){
+            float rAng = rand() / RAND_MAX * 360; // (0, 360)
+            float rn = -0.35 + rand() / RAND_MAX * (0.35 - (-0.35)); // (-0.35, 0.35)
+            
+            // Distancia
+            float _min_distance = sdlutils().width() + sdlutils().width() * 0.1;
+            float _op_dist = _min_distance * (-rn);
+
+            // Medio de la pantalla + angulo * distancia
+            Vector2D posVec = Vector2D(sdlutils().width() / 2 + cos(rAng) * (_min_distance + _op_dist), sdlutils().height() / 2 + sin(rAng) * (_min_distance + _op_dist));
+
+            // spawnEnemy(posVec); //!!!!!!!!!!
+            //_enemyPool->addEnemies(x, y); //!!!!!!!!!!
+            _enemiesSpawned++;
+
+            // Tiempo
+            _min_time = _totalSpawnTime / _numEnemies;
+            _op_time = _min_time * rn;
+            _nextSpawn = currentTime + (_min_time + _op_time);
+        }
+    }
+    else _waveActive = true; // despuÃ©s de que se spawnee el Ãºltimo enemigo
+    _endWaveTime = currentTime + _totalSpawnTime; // se calcula cuÃ¡ndo va a terminar la oleada
+}
+
+//Verifica si todos los enemigos estan muertos
+bool 
+WaveManager::areAllEnemiesDead() {
+    auto activeEnemies = _enemyPool->getActiveObjects();
+    return activeEnemies.empty();
+}
+
+//Activa la niebla
+void 
+WaveManager::activateFog() {
+    _fogActive = true;
+    std::cout << "Niebla activada!" << std::endl;
+}
+
+
+void 
+WaveManager::enterRewardsMenu() {
+    std::cout << "Todos los enemigos eliminados. Entrando al menu de recompensas..." << std::endl;
+}
+
--- project/src/our_scripts/WaveManager.h
+++ project/src/our_scripts/WaveManager.h
@@ -1,28 +1,42 @@
-//#pragma once
-//#include "../ecs/Manager.h"
-//
-//
-//class EnemyPoolManager;
-//
-//class WaveManager {
-//public:
-//    WaveManager(ecs::Manager* mngr, EnemyPoolManager* enemyPool);
-//
-//    void update();
-//
-//    void spawnWave();
-//
-//    bool areAllEnemiesDead();
-//
-//    void activateFog();
-//
-//    void enterRewardsMenu();
-//
-//private:
-//    ecs::Manager* _mngr;
-//    EnemyPoolManager* _enemyPool;
-//    Uint32 _lastSpawnTime;
-//    Uint32 _spawnInterval;
-//    bool _waveActive;
-//    bool _fogActive;
-//};
+#pragma once
+#include "../ecs/Manager.h"
+
+
+class EnemyPoolManager;
+
+class WaveManager {
+public:
+    WaveManager(ecs::Manager* mngr, EnemyPoolManager* enemyPool);
+
+    void update();
+
+    void spawnWave();
+
+    bool areAllEnemiesDead();
+
+    void activateFog();
+
+    void enterRewardsMenu();
+
+private:
+    ecs::Manager* _mngr;
+    EnemyPoolManager* _enemyPool;
+
+    Uint32 currentTime; //tiempo actual (post calculo)
+    Uint32 _waveTime; // cuánto dura la oleada (CONSTRUCTOR)
+    Uint32 _endWaveTime; // cuándo termina la oleada (post calculo)
+
+
+    bool _waveActive = false;
+    bool _fogActive = false;
+
+    int _numEnemies; // enemigos total en la oleada (CONSTRUCTOR)
+    int _enemiesSpawned; // número de enemigos spawneados (post calculo)
+
+    float _totalSpawnTime; // tiempo spawn total al prinicipio de la oleada (CONSTRUCTOR)
+    float _nextSpawn; // cuándo se spawnea el siguiente enemigo (post calculo)
+
+    // tiempo de spawn del siguiente enemigo (post calculo)
+    float _min_time;
+    float _op_time;
+};
