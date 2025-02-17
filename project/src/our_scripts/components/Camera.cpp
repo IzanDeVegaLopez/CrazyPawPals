c907079311d9a8511114e38454ecfb97b07bba88
M00nHun7r3ss <denisaju@ucm.es>
2025-02-17 16:14:21 +0100

    Movimiento arreglado

--- project/src/our_scripts/components/Camera.cpp
+++ project/src/our_scripts/components/Camera.cpp
@@ -1,20 +1,20 @@
-#include "Camera.h"
-#include "../sdlutils/SDLUtils.h"
-#include "../ecs/Manager.h"
-
-#include "Transform.h"
-#include "KeyboardPlayerCtrl.h"
-#include "MovementController.h"
-
-Camera::Camera(ecs::Manager* mngr) : GameObject::GameObject(mngr, ecs::grp::CAMERA), _maxSpeed(5.0f)
-{
-	_tr = _entity->getMngr()->getComponent<Transform>(_entity);
-	float s = 100.0f;
-	_tr->init({ sdlutils().width() / 2.0f, sdlutils().height() / 2.0f }, { 0.0f,0.0f }, { 0.0f,0.0f }, s, s, 0.0f, 2.0f);
-	_entity->getMngr()->addComponent<KeyboardPlayerCtrl>(_entity);
-	_entity->getMngr()->addComponent<MovementController>(_entity);
-}
-
-Camera::~Camera() {}
-
-
+//#include "Camera.h"
+////#include "../sdlutils/SDLUtils.h"
+////#include "../ecs/Manager.h"
+//
+//#include "Transform.h"
+//#include "KeyboardPlayerCtrl.h"
+//#include "MovementController.h"
+//
+//Camera::Camera(ecs::Manager* mngr) : GameObject::GameObject(mngr, ecs::grp::CAMERA), _maxSpeed(5.0f)
+//{
+//	_tr = _entity->getMngr()->getComponent<Transform>(_entity);
+//	float s = 100.0f;
+//	_tr->init({ sdlutils().width() / 2.0f, sdlutils().height() / 2.0f }, { 0.0f,0.0f }, { 0.0f,0.0f }, s, s, 0.0f, 2.0f);
+//	_entity->getMngr()->addComponent<KeyboardPlayerCtrl>(_entity);
+//	_entity->getMngr()->addComponent<MovementController>(_entity);
+//}
+//
+//Camera::~Camera() {}
+//
+//
--- project/src/our_scripts/components/Camera.h
+++ project/src/our_scripts/components/Camera.h
@@ -1,12 +1,12 @@
-#pragma once
-#include "../../game/GameObject.h"
-
-class Camera : public GameObject {
-
-protected:
-	Transform* _tr;
-
-public:
-	Camera(ecs::Manager* mngr);
-	virtual ~Camera() {};
-};
+//#pragma once
+//#include "../../game/GameObject.h"
+//
+//class Camera : public GameObject {
+//
+//protected:
+//	Transform* _tr;
+//
+//public:
+//	Camera(ecs::Manager* mngr);
+//	virtual ~Camera() {};
+//};
