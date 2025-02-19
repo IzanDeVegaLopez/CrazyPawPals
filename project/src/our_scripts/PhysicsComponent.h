5c614a2410d19d47e74cf54159352084b7ddd8a7
MaEduardaB <157380163+MaEduardaB@users.noreply.github.com>
2025-02-19 17:30:58 +0100

    Muchos errores, y arreglo de muchos conflictos

--- project/src/our_scripts/PhysicsComponent.h
+++ project/src/our_scripts/PhysicsComponent.h
@@ -1,11 +1,11 @@
 // This file is part of the course TPV2@UCM - Samir Genaim
 
 #pragma once
-#include "Component.h"
+#include "../ecs/Component.h"
 
 class Container;
 
-class PhysicsComponent: public Component {
+class PhysicsComponent: ecs::Component {
 public:
 	PhysicsComponent() {
 	}
