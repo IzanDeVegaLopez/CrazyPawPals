4202ec4e87be4c45d6f6c3dd03fd0a6a341d64ab
Izan <izandeve@ucm.es>
2025-03-01 18:16:25 +0100

    bombardeen c++ pq no le sabe a los eventos

--- project/src/utils/EventsSystem.hpp
+++ project/src/utils/EventsSystem.hpp
@@ -1,62 +1,44 @@
+#include <stdio.h>
+
 namespace ev_sys {
 
 	struct event_info {
-		int irrelevant_test_data;
+		int irrelevant_test_data = 0;
 	};
 
-	delegate void Del(int, float);
+	[event_source(_NATIVE_NULLPTR_SUPPORTED)]
+	class event_source {
+	public:
+		__event void my_event(event_info& ei);
+	};
 
-	//Inherit from event_receiver in order to be able to consume events
+	[event_receiver(_NATIVE_NULLPTR_SUPPORTED)]
 	class event_receiver {
 	public:
 		virtual void handler(event_info&) = 0;
-	};
-		
-	//To create new functions inherit from event_receiver
-	//and override handler
-
-
-	interface struct event_triggerer_interface {
-	public:
-		event Del^ E;
-		void fire_event(event_info&);
-	};
-
-	//Obligatoriamente ha de crearse con gcnew por ser "ref"
-	ref class event_source : public event_triggerer_interface {
-		virtual event Del^ E;
-		virtual void fire_event(event_info& e) {
-			E(e);
+		void hook_event(event_source* es) {
+			__hook(&event_source::my_event, es, &event_receiver::handler);
 		}
-		void suscribe(event_receiver er) {
-			E += gcnew Del(er, &event_receiver::handler);
-		}
-		void desuscribe(event_receiver er) {
-			E -= gcnew Del(er, &event_receiver::handler);
+		void unhook_event(event_source* es) {
+			__unhook(&event_source::my_event, es, &event_receiver::handler);
 		}
 	};
 }
 
 
 //Case of Use
-//Uncomment to see better
 /*
 int main() {
-	//elements created by gcnew 
-	//are automatically garbage-collected
-	//aka they must not be deleted manually
-	
-	//create event triggerer (^ means that it will be created by gcnew)
-	//Store as many event triggerers in the class that will fire them
-	event_triggerer_interface^ _eti = gcnew event_source();
-	//create event receiver
-	event_receiver^ _er = new event_receiver();
+	ev_sys::event_source source;
+	ev_sys::event_receiver receiver;
 
-	//suscribe
-	_eti->E += gcnew Del(_er, &event_receiver::handler);
-	//call the vent
-	_eti->fire_event(event_info{ 1 });
-	//desuscribe
-	_eti->E -= gcnew Del(_er, &event_receiver::handler);
+	//suscribe to event
+	receiver.hook_event(&source);
+	//invoke event
+	ev_sys::event_info ev = ev_sys::event_info();
+	ev.irrelevant_test_data = 0;
+	__raise source.my_event(ev);
+	//unsuscribe to event
+	receiver.unhook_event(&source);
 }
 */
