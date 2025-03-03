6b06871941b32bff1e74de9e4c4973f934836423
Izan <izandeve@ucm.es>
2025-03-03 17:59:31 +0100

    card now receives a pointer to mouse poss

--- project/src/our_scripts/components/KeyboardPlayerCtrl.cpp
+++ project/src/our_scripts/components/KeyboardPlayerCtrl.cpp
@@ -13,7 +13,7 @@
 KeyboardPlayerCtrl::KeyboardPlayerCtrl()
     : _left(SDL_SCANCODE_A), _right(SDL_SCANCODE_D), _up(SDL_SCANCODE_W), _down(SDL_SCANCODE_S), 
       _reload(SDL_SCANCODE_SPACE), _collect(SDL_SCANCODE_F), 
-      _mc(nullptr), _w(nullptr), _dc(nullptr) {}
+      _mc(nullptr), _w(nullptr), _dc(nullptr), _mouse_pos(Vector2D(0,0)) {}
 
 KeyboardPlayerCtrl::~KeyboardPlayerCtrl() {}
 
@@ -59,20 +59,23 @@ void KeyboardPlayerCtrl::update(Uint32 delta_time) {
         //std::cout << "colecta" << std::endl;
     }
 
+    auto _mouse_rect =rect_f32_global_from_screen_rect_flipped_y(
+        {
+            {
+                ih().getMousePos().getX(),
+                ih().getMousePos().getY()
+            },
+            {
+                1,1
+            }
+        },
+        Game::Instance()->get_mngr()->getComponent<camera_component>(Game::Instance()->get_mngr()->getHandler(ecs::hdlr::CAMERA))->cam
+    );
+    _mouse_pos = Vector2D(_mouse_rect.position.x, _mouse_rect.position.y);
+
 
     if (ihdlr.mouseButtonDownEvent()) {
-        rect_f32 converted_mouse_pos = rect_f32_global_from_screen_rect_flipped_y(
-            {
-                {
-                    ih().getMousePos().getX(),
-                    ih().getMousePos().getY()
-                },
-                {
-                    1,1
-                }
-            },
-            Game::Instance()->get_mngr()->getComponent<camera_component>(Game::Instance()->get_mngr()->getHandler(ecs::hdlr::CAMERA))->cam
-        );
+
         //Vector2D mousePos = { (float)ihdlr.getMousePos().getX(), (float)ihdlr.getMousePos().getY()};
         //shoot
         if (ihdlr.getMouseButtonState(InputHandler::LEFT)) {
@@ -80,14 +83,14 @@ void KeyboardPlayerCtrl::update(Uint32 delta_time) {
             //_w->shoot(mousePos);
             if (_dc->discard_card()) {
                 //position2_f32 mouse_pos = Game::Instance()->get_mngr()->getComponent<camera_component>(Game::Instance()->get_mngr()->getHandler(ecs::hdlr::CAMERA))->mouse_world_position; 
-                _w->shoot(Vector2D{ converted_mouse_pos.position.x, converted_mouse_pos.position.y });
+                _w->shoot(_mouse_pos);
             }       
         }
         //use card
         else if (ihdlr.getMouseButtonState(InputHandler::RIGHT)) {
             //send message to use a card
-            Vector2D* vec = new Vector2D{ converted_mouse_pos.position.x,  converted_mouse_pos.position.y };
-            _dc->use_card(vec);
+            //Vector2D* vec = new Vector2D{ converted_mouse_pos.position.x,  converted_mouse_pos.position.y };
+            _dc->use_card(&_mouse_pos);
         }
     }
     
