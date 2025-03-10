3a7a610790fb03084fbdff133adc2507aafc171b
MaEduardaB <157380163+MaEduardaB@users.noreply.github.com>
2025-03-10 18:44:49 +0100

    incio catkuza

---
+++ project/src/our_scripts/components/WeaponCatKuza.cpp
@@ -0,0 +1,63 @@
+#include "WeaponCatKuza.h"
+#include "../../game/Game.h"
+#include "../../game/GameScene.h"
+#include "../card_system/ShootPatrons.hpp"
+
+WeaponCatKuza::WeaponCatKuza() : Weapon(4, 5000, 20.0f, 0.1f, "p_plimplim", 1.0f, 1.0f), _state(0), _wind_p(3), _area_p(6), _dash_p(6) { }
+
+WeaponCatKuza::~WeaponCatKuza() {}
+
+void 
+WeaponCatKuza::callback(Vector2D shootPos, Vector2D shootDir) {
+	GameStructs::BulletProperties bp = GameStructs::BulletProperties();
+	bp.dir = shootDir;
+	bp.init_pos = shootPos;
+
+	callback_3_wind(bp);
+
+	// switch (expression)
+	// {
+	// case constant expression:
+	
+	// 	break;
+	
+	// default:
+	// 	break;
+	// }
+
+
+	float totalAngle = 60.0f;
+
+	patrons::ShotgunPatron(bp, ecs::grp::BULLET, totalAngle, _state);
+
+}
+
+void WeaponCatKuza::callback_3_wind(GameStructs::BulletProperties &bp){
+	bp.speed =_speed;
+	bp.damage = _damage;
+	bp.life_time = 2;
+	bp.width = _attack_width* 0.5f;
+	bp.height = _attack_height * 0.5f;
+
+	bp.sprite_key = "p_plimplim";
+}
+
+void WeaponCatKuza::callback_area(GameStructs::BulletProperties &bp){
+	bp.speed =_speed;
+	bp.damage = _damage;
+	bp.life_time = 2;
+	bp.width = _attack_width* 0.5f;
+	bp.height = _attack_height * 0.5f;
+
+	bp.sprite_key = "p_plimplim";
+}
+
+void WeaponCatKuza::callback_dash(GameStructs::BulletProperties &bp){
+	bp.speed =_speed;
+	bp.damage = _damage;
+	bp.life_time = 2;
+	bp.width = _attack_width* 0.5f;
+	bp.height = _attack_height * 0.5f;
+
+	bp.sprite_key = "p_plimplim";
+}
---
+++ project/src/our_scripts/components/WeaponCatKuza.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Weapon.h"
+
+class WeaponCatKuza : public Weapon {
+protected:
+	void callback(Vector2D shootPos, Vector2D shootDir) override;
+public:
+	__CMPID_DECL__(ecs::cmp::WEAPON);
+	WeaponCatKuza();
+	virtual ~WeaponCatKuza();
+
+private:
+	void callback_3_wind(GameStructs::BulletProperties &bp);
+	void callback_area(GameStructs::BulletProperties &bp);
+	void callback_dash(GameStructs::BulletProperties &bp);
+
+	int _state;
+	int _wind_p;
+	int _area_p;
+	int _dash_p;
+};
