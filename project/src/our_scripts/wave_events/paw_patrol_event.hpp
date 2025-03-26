4e93d9cf43b693f268588133143269ccb8b8b743
JaviFueyo <jfueyo@ucm.es>
2025-03-26 17:57:19 +0100

    No habia creado el .hpp del evento jaja
    Aiuda

---
+++ project/src/our_scripts/wave_events/paw_patrol_event.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "../components/WaveManager.h"
+#include "../../utils/EventsSystem.hpp"
+class paw_patrol_event : public wave_event {
+public:
+	inline paw_patrol_event(WaveManager* wav)
+		:wave_event(wav)
+	{
+	}
+
+	inline void start_wave_callback() override {
+		wave_event::start_wave_callback();
+		event_system::event_manager::Instance()->fire_event(event_system::paw_patrol,
+			(event_system::event_receiver::Msg{}));
+		//TODO: send event for everyone out there
+	}
+	inline void end_wave_callback() override {
+		//TODO: send event to revert
+		event_system::event_manager::Instance()->fire_event(event_system::paw_patrol,
+			(event_system::event_receiver::Msg{}));
+	}
+};
